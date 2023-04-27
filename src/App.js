391350dc8fea7960cee3362435b18a3c58a1707d
Mihkel Tiganik <mtiganik@gmail.com>
2023-04-27 17:49:01 +0300

    start multiselect plugin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react'
+
+const categories = ["sport", "kunst","haridus"]
 
 function App() {
+  const [text, setText] = React.useState("")
+
+  function handleClick(){
+    setText("You pressed me")
+  }
   return (
     <div className="App">
-      <div>Hello world</div>
+      <div>Vali kategooria</div>
+      
+      <button onClick={handleClick}>Push me</button>
+      <div>{text}</div>
+      <p value={text} />
     </div>
   );
 }
