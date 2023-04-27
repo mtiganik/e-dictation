ccdf15001a045c62edca9cceb384f04e2cbb9511
Mihkel Tiganik <mtiganik@gmail.com>
2023-04-27 18:29:43 +0300

    start to http requests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react'
 
-const categories = ["sport", "kunst","haridus"]
+// const categories = ["sport", "kunst", "haridus"]
+// const defaultCategory = categories[0]
+
+
+const options = [
+  { label: 'Sport', value: 'sport' },
+  { label: 'Art', value: 'kunst' },
+  { label: 'Education', value: 'haridus' },
+
+];
 
 function App() {
   const [text, setText] = React.useState("")
+  const [category, setCategory] = React.useState(options[0].value);  
 
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+ 
   function handleClick(){
-    setText("You pressed me")
+    const textToDisplay = "We will look for e-dictation in category: " + category
+    setText(textToDisplay)
   }
   return (
-    <div className="App">
+    <div >
       <div>Vali kategooria</div>
-      
-      <button onClick={handleClick}>Push me</button>
+      <select value={category} onChange={handleCategoryChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>{option.label}</option>
+          ))}
+      </select>
+
+      <button onClick={handleClick}>leia etteütlus</button>
       <div>{text}</div>
-      <p value={text} />
     </div>
   );
 }
